7ade0b5c3413e2fe9a15898b8923504ab099d555
SanderSaska <sander.saska@gmail.com>
2025-12-20 22:45:33 +0200

    Refactor training script to integrate DiceCrossEntropyLoss and improve logging format

--- RGBX/train.py
+++ RGBX/train.py
@@ -22,11 +22,12 @@ from engine.logger import get_logger
 from utils.pyt_utils import all_reduce_tensor
 
 from tensorboardX import SummaryWriter
+from utils.loss_opr import DiceCrossEntropyLoss
 
 parser = argparse.ArgumentParser()
 logger = get_logger()
 
-os.environ['MASTER_PORT'] = '169710'
+os.environ["MASTER_PORT"] = "169710"
 
 with Engine(custom_parser=parser) as engine:
     args = parser.parse_args()
@@ -43,65 +44,90 @@ with Engine(custom_parser=parser) as engine:
     train_loader, train_sampler = get_train_loader(engine, RGBXDataset)
 
     if (engine.distributed and (engine.local_rank == 0)) or (not engine.distributed):
-        tb_dir = config.tb_dir + '/{}'.format(time.strftime("%b%d_%d-%H-%M", time.localtime()))
-        generate_tb_dir = config.tb_dir + '/tb'
+        tb_dir = config.tb_dir + "/{}".format(
+            time.strftime("%b%d_%d-%H-%M", time.localtime())
+        )
+        generate_tb_dir = config.tb_dir + "/tb"
         tb = SummaryWriter(log_dir=tb_dir)
         engine.link_tb(tb_dir, generate_tb_dir)
 
     # config network and criterion
-    criterion = nn.CrossEntropyLoss(reduction='mean', ignore_index=config.background)
+    # Use 255 as ignore_index (safe sentinel not present in remapped 0/1 masks)
+    if getattr(config, "loss_type", "dice_ce") == "dice_ce":
+        criterion = DiceCrossEntropyLoss(dice_weight=config.dice_weight, ce_weight=config.ce_weight, ignore_index=255)
+    else:
+        criterion = nn.CrossEntropyLoss(reduction="mean", ignore_index=255)
 
     if engine.distributed:
         BatchNorm2d = nn.SyncBatchNorm
     else:
         BatchNorm2d = nn.BatchNorm2d
-    
-    model=segmodel(cfg=config, criterion=criterion, norm_layer=BatchNorm2d)
-    
+
+    model = segmodel(cfg=config, criterion=criterion, norm_layer=BatchNorm2d)
+
     # group weight and config optimizer
     base_lr = config.lr
     if engine.distributed:
         base_lr = config.lr
-    
+
     params_list = []
     params_list = group_weight(params_list, model, BatchNorm2d, base_lr)
-    
-    if config.optimizer == 'AdamW':
-        optimizer = torch.optim.AdamW(params_list, lr=base_lr, betas=(0.9, 0.999), weight_decay=config.weight_decay)
-    elif config.optimizer == 'SGDM':
-        optimizer = torch.optim.SGD(params_list, lr=base_lr, momentum=config.momentum, weight_decay=config.weight_decay)
+
+    if config.optimizer == "AdamW":
+        optimizer = torch.optim.AdamW(
+            params_list,
+            lr=base_lr,
+            betas=(0.9, 0.999),
+            weight_decay=config.weight_decay,
+        )
+    elif config.optimizer == "SGDM":
+        optimizer = torch.optim.SGD(
+            params_list,
+            lr=base_lr,
+            momentum=config.momentum,
+            weight_decay=config.weight_decay,
+        )
     else:
         raise NotImplementedError
 
     # config lr policy
     total_iteration = config.nepochs * config.niters_per_epoch
-    lr_policy = WarmUpPolyLR(base_lr, config.lr_power, total_iteration, config.niters_per_epoch * config.warm_up_epoch)
+    lr_policy = WarmUpPolyLR(
+        base_lr,
+        config.lr_power,
+        total_iteration,
+        config.niters_per_epoch * config.warm_up_epoch,
+    )
 
     if engine.distributed:
-        logger.info('.............distributed training.............')
+        logger.info(".............distributed training.............")
         if torch.cuda.is_available():
             model.cuda()
-            model = DistributedDataParallel(model, device_ids=[engine.local_rank], 
-                                            output_device=engine.local_rank, find_unused_parameters=False)
+            model = DistributedDataParallel(
+                model,
+                device_ids=[engine.local_rank],
+                output_device=engine.local_rank,
+                find_unused_parameters=False,
+            )
     else:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
 
-    engine.register_state(dataloader=train_loader, model=model,
-                          optimizer=optimizer)
+    engine.register_state(dataloader=train_loader, model=model, optimizer=optimizer)
     if engine.continue_state_object:
         engine.restore_checkpoint()
 
     optimizer.zero_grad()
     model.train()
-    logger.info('begin trainning:')
-    
-    for epoch in range(engine.state.epoch, config.nepochs+1):
+    logger.info("begin trainning:")
+
+    for epoch in range(engine.state.epoch, config.nepochs + 1):
         if engine.distributed:
             train_sampler.set_epoch(epoch)
-        bar_format = '{desc}[{elapsed}<{remaining},{rate_fmt}]'
-        pbar = tqdm(range(config.niters_per_epoch), file=sys.stdout,
-                    bar_format=bar_format)
+        bar_format = "{desc}[{elapsed}<{remaining},{rate_fmt}]"
+        pbar = tqdm(
+            range(config.niters_per_epoch), file=sys.stdout, bar_format=bar_format
+        )
         dataloader = iter(train_loader)
 
         sum_loss = 0
@@ -109,10 +135,10 @@ with Engine(custom_parser=parser) as engine:
         for idx in pbar:
             engine.update_iteration(epoch, idx)
 
-            minibatch = dataloader.next()
-            imgs = minibatch['data']
-            gts = minibatch['label']
-            modal_xs = minibatch['modal_x']
+            minibatch = next(dataloader)
+            imgs = minibatch["data"]
+            gts = minibatch["label"]
+            modal_xs = minibatch["modal_x"]
 
             imgs = imgs.cuda(non_blocking=True)
             gts = gts.cuda(non_blocking=True)
@@ -121,45 +147,71 @@ with Engine(custom_parser=parser) as engine:
             aux_rate = 0.2
             loss = model(imgs, modal_xs, gts)
 
+            # per-batch downweight if tile is overwhelmingly positive
+            try:
+                # exclude ignore_index (255) from the positive fraction calculation
+                valid_mask = (gts != 255)
+                valid_count = float(valid_mask.float().sum().item())
+                if valid_count == 0:
+                    pos_frac = 0.0
+                else:
+                    pos_frac = float(((gts > 0) & valid_mask).float().sum().item() / valid_count)
+            except Exception:
+                pos_frac = 0.0
+            if pos_frac >= config.full_pos_frac_threshold:
+                down = float(config.full_pos_downweight)
+                loss = loss * down
+
             # reduce the whole loss over multi-gpu
             if engine.distributed:
                 reduce_loss = all_reduce_tensor(loss, world_size=engine.world_size)
-            
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            current_idx = (epoch- 1) * config.niters_per_epoch + idx 
+            current_idx = (epoch - 1) * config.niters_per_epoch + idx
             lr = lr_policy.get_lr(current_idx)
 
             for i in range(len(optimizer.param_groups)):
-                optimizer.param_groups[i]['lr'] = lr
+                optimizer.param_groups[i]["lr"] = lr
 
             if engine.distributed:
                 sum_loss += reduce_loss.item()
-                print_str = 'Epoch {}/{}'.format(epoch, config.nepochs) \
-                        + ' Iter {}/{}:'.format(idx + 1, config.niters_per_epoch) \
-                        + ' lr=%.4e' % lr \
-                        + ' loss=%.4f total_loss=%.4f' % (reduce_loss.item(), (sum_loss / (idx + 1)))
+                print_str = (
+                    "Epoch {}/{}".format(epoch, config.nepochs)
+                    + " Iter {}/{}:".format(idx + 1, config.niters_per_epoch)
+                    + " lr=%.4e" % lr
+                    + " loss=%.4f total_loss=%.4f"
+                    % (reduce_loss.item(), (sum_loss / (idx + 1)))
+                )
             else:
-                sum_loss += loss
-                print_str = 'Epoch {}/{}'.format(epoch, config.nepochs) \
-                        + ' Iter {}/{}:'.format(idx + 1, config.niters_per_epoch) \
-                        + ' lr=%.4e' % lr \
-                        + ' loss=%.4f total_loss=%.4f' % (loss, (sum_loss / (idx + 1)))
+                sum_loss += loss.item()
+                print_str = (
+                    "Epoch {}/{}".format(epoch, config.nepochs)
+                    + " Iter {}/{}:".format(idx + 1, config.niters_per_epoch)
+                    + " lr=%.4e" % lr
+                    + " loss=%.4f total_loss=%.4f" % (loss.item(), (sum_loss / (idx + 1)))
+                )
 
             del loss
             pbar.set_description(print_str, refresh=False)
-        
-        if (engine.distributed and (engine.local_rank == 0)) or (not engine.distributed):
-            tb.add_scalar('train_loss', sum_loss / len(pbar), epoch)
 
-        if (epoch >= config.checkpoint_start_epoch) and (epoch % config.checkpoint_step == 0) or (epoch == config.nepochs):
+        if (engine.distributed and (engine.local_rank == 0)) or (
+            not engine.distributed
+        ):
+            tb.add_scalar("train_loss", sum_loss / len(pbar), epoch)
+
+        if (
+            (epoch >= config.checkpoint_start_epoch)
+            and (epoch % config.checkpoint_step == 0)
+            or (epoch == config.nepochs)
+        ):
             if engine.distributed and (engine.local_rank == 0):
-                engine.save_and_link_checkpoint(config.checkpoint_dir,
-                                                config.log_dir,
-                                                config.log_dir_link)
+                engine.save_and_link_checkpoint(
+                    config.checkpoint_dir, config.log_dir, config.log_dir_link
+                )
             elif not engine.distributed:
-                engine.save_and_link_checkpoint(config.checkpoint_dir,
-                                                config.log_dir,
-                                                config.log_dir_link)
+                engine.save_and_link_checkpoint(
+                    config.checkpoint_dir, config.log_dir, config.log_dir_link
+                )
