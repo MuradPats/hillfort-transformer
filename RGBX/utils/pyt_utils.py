1ba89ea8dbf07417dc63e9a597d392db23298a30
SanderSaska <sander.saska@gmail.com>
2026-01-21 20:00:59 +0200

    Improve model loading logic for better compatibility

--- RGBX/utils/pyt_utils.py
+++ RGBX/utils/pyt_utils.py
@@ -11,24 +11,25 @@ import torch
 import torch.utils.model_zoo as model_zoo
 import torch.distributed as dist
 
+
 class LogFormatter(logging.Formatter):
     log_fout = None
-    date_full = '[%(asctime)s %(lineno)d@%(filename)s:%(name)s] '
-    date = '%(asctime)s '
-    msg = '%(message)s'
+    date_full = "[%(asctime)s %(lineno)d@%(filename)s:%(name)s] "
+    date = "%(asctime)s "
+    msg = "%(message)s"
 
     def format(self, record):
         if record.levelno == logging.DEBUG:
-            mcl, mtxt = self._color_dbg, 'DBG'
+            mcl, mtxt = self._color_dbg, "DBG"
         elif record.levelno == logging.WARNING:
-            mcl, mtxt = self._color_warn, 'WRN'
+            mcl, mtxt = self._color_warn, "WRN"
         elif record.levelno == logging.ERROR:
-            mcl, mtxt = self._color_err, 'ERR'
+            mcl, mtxt = self._color_err, "ERR"
         else:
-            mcl, mtxt = self._color_normal, ''
+            mcl, mtxt = self._color_normal, ""
 
         if mtxt:
-            mtxt += ' '
+            mtxt += " "
 
         if self.log_fout:
             self.__set_fmt(self.date_full + mtxt + self.msg)
@@ -44,27 +45,29 @@ class LogFormatter(logging.Formatter):
         return formatted
 
     if sys.version_info.major < 3:
+
         def __set_fmt(self, fmt):
             self._fmt = fmt
     else:
+
         def __set_fmt(self, fmt):
             self._style._fmt = fmt
 
     @staticmethod
     def _color_dbg(msg):
-        return '\x1b[36m{}\x1b[0m'.format(msg)
+        return "\x1b[36m{}\x1b[0m".format(msg)
 
     @staticmethod
     def _color_warn(msg):
-        return '\x1b[1;31m{}\x1b[0m'.format(msg)
+        return "\x1b[1;31m{}\x1b[0m".format(msg)
 
     @staticmethod
     def _color_err(msg):
-        return '\x1b[1;4;31m{}\x1b[0m'.format(msg)
+        return "\x1b[1;4;31m{}\x1b[0m".format(msg)
 
     @staticmethod
     def _color_omitted(msg):
-        return '\x1b[35m{}\x1b[0m'.format(msg)
+        return "\x1b[35m{}\x1b[0m".format(msg)
 
     @staticmethod
     def _color_normal(msg):
@@ -72,10 +75,12 @@ class LogFormatter(logging.Formatter):
 
     @staticmethod
     def _color_date(msg):
-        return '\x1b[32m{}\x1b[0m'.format(msg)
+        return "\x1b[32m{}\x1b[0m".format(msg)
 
-_default_level_name = os.getenv('ENGINE_LOGGING_LEVEL', 'INFO')
+
+_default_level_name = os.getenv("ENGINE_LOGGING_LEVEL", "INFO")
 _default_level = logging.getLevelName(_default_level_name.upper())
+
 
 def get_logger(log_dir=None, log_file=None, formatter=LogFormatter):
     logger = logging.getLogger()
@@ -85,25 +90,26 @@ def get_logger(log_dir=None, log_file=None, formatter=LogFormatter):
     if log_dir and log_file:
         ensure_dir(log_dir)
         LogFormatter.log_fout = True
-        file_handler = logging.FileHandler(log_file, mode='a')
+        file_handler = logging.FileHandler(log_file, mode="a")
         file_handler.setLevel(logging.INFO)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter(datefmt='%d %H:%M:%S'))
+    stream_handler.setFormatter(formatter(datefmt="%d %H:%M:%S"))
     stream_handler.setLevel(0)
     logger.addHandler(stream_handler)
     return logger
 
+
 logger = get_logger()
 
 model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+    "resnet18": "https://download.pytorch.org/models/resnet18-5c106cde.pth",
+    "resnet34": "https://download.pytorch.org/models/resnet34-333f7ec4.pth",
+    "resnet50": "https://download.pytorch.org/models/resnet50-19c8e357.pth",
+    "resnet101": "https://download.pytorch.org/models/resnet101-5d3b4d8f.pth",
+    "resnet152": "https://download.pytorch.org/models/resnet152-b121ed2d.pth",
 }
 
 
@@ -132,25 +138,59 @@ def load_restore_model(model, model_file):
 
     if isinstance(model_file, str):
         state_dict = torch.load(model_file)
-        if 'model' in state_dict.keys():
-            state_dict = state_dict['model']
-        elif 'state_dict' in state_dict.keys():
-            state_dict = state_dict['state_dict']
-        elif 'module' in state_dict.keys():
-            state_dict = state_dict['module']
+        if "model" in state_dict.keys():
+            state_dict = state_dict["model"]
+        elif "state_dict" in state_dict.keys():
+            state_dict = state_dict["state_dict"]
+        elif "module" in state_dict.keys():
+            state_dict = state_dict["module"]
     else:
         state_dict = model_file
     t_ioend = time.time()
 
-    model.load_state_dict(state_dict, strict=True)
-    
+    # Align checkpoint and model key prefixes (handle 'module.' from DDP/DataParallel)
+    ckpt_keys = list(state_dict.keys())
+    model_keys = list(model.state_dict().keys())
+
+    def _has_module_prefix(keys):
+        return any(k.startswith("module.") for k in keys)
+
+    ckpt_has_module = _has_module_prefix(ckpt_keys)
+    model_has_module = _has_module_prefix(model_keys)
+
+    if ckpt_has_module and not model_has_module:
+        mapped = OrderedDict()
+        for k, v in state_dict.items():
+            nk = k[len("module.") :] if k.startswith("module.") else k
+            mapped[nk] = v
+        state_dict = mapped
+    elif (not ckpt_has_module) and model_has_module:
+        mapped = OrderedDict()
+        for k, v in state_dict.items():
+            nk = "module." + k
+            mapped[nk] = v
+        state_dict = mapped
+
+    # Try strict load first; fall back to strict=False on failure
+    try:
+        model.load_state_dict(state_dict, strict=True)
+    except RuntimeError as e:
+        logger.warning(
+            "Strict load_restore_model failed (%s). Falling back to strict=False.",
+            str(e),
+        )
+        model.load_state_dict(state_dict, strict=False)
+
     del state_dict
     t_end = time.time()
     logger.info(
         "Load model, Time usage:\n\tIO: {}, initialize parameters: {}".format(
-            t_ioend - t_start, t_end - t_ioend))
+            t_ioend - t_start, t_end - t_ioend
+        )
+    )
 
     return model
+
 
 def load_model(model, model_file, is_restore=False):
     t_start = time.time()
@@ -160,48 +200,98 @@ def load_model(model, model_file, is_restore=False):
 
     if isinstance(model_file, str):
         state_dict = torch.load(model_file)
-        if 'model' in state_dict.keys():
-            state_dict = state_dict['model']
-        elif 'state_dict' in state_dict.keys():
-            state_dict = state_dict['state_dict']
-        elif 'module' in state_dict.keys():
-            state_dict = state_dict['module']
+        if "model" in state_dict.keys():
+            state_dict = state_dict["model"]
+        elif "state_dict" in state_dict.keys():
+            state_dict = state_dict["state_dict"]
+        elif "module" in state_dict.keys():
+            state_dict = state_dict["module"]
     else:
         state_dict = model_file
     t_ioend = time.time()
 
-    if is_restore:
-        new_state_dict = OrderedDict()
-        for k, v in state_dict.items():
-            name = 'module.' + k
-            new_state_dict[name] = v
-        state_dict = new_state_dict
+    # The earlier behaviour tried to add a 'module.' prefix when is_restore==True.
+    # Instead, use a robust alignment strategy below to handle 'module.' mismatches
+    # regardless of the is_restore flag.
 
-    model.load_state_dict(state_dict, strict=True)
-    ckpt_keys = set(state_dict.keys())
-    own_keys = set(model.state_dict().keys())
-    missing_keys = own_keys - ckpt_keys
-    unexpected_keys = ckpt_keys - own_keys
+    # Align checkpoint and model key prefixes (handle 'module.' from DDP/DataParallel)
+    ckpt_keys = list(state_dict.keys())
+    model_keys = list(model.state_dict().keys())
+
+    def _has_module_prefix(keys):
+        return any(k.startswith("module.") for k in keys)
+
+    ckpt_has_module = _has_module_prefix(ckpt_keys)
+    model_has_module = _has_module_prefix(model_keys)
+
+    # Create a mapped state dict so keys match model.state_dict()
+    if ckpt_has_module and not model_has_module:
+        # strip 'module.' from checkpoint keys
+        mapped = OrderedDict()
+        for k, v in state_dict.items():
+            nk = k[len("module.") :] if k.startswith("module.") else k
+            mapped[nk] = v
+        state_dict = mapped
+    elif (not ckpt_has_module) and model_has_module:
+        # add 'module.' prefix to checkpoint keys
+        mapped = OrderedDict()
+        for k, v in state_dict.items():
+            nk = "module." + k
+            mapped[nk] = v
+        state_dict = mapped
+
+    # Try strict load first; fall back to strict=False if mismatch occurs
+    try:
+        model.load_state_dict(state_dict, strict=True)
+        ckpt_keys = set(state_dict.keys())
+        own_keys = set(model.state_dict().keys())
+        missing_keys = own_keys - ckpt_keys
+        unexpected_keys = ckpt_keys - own_keys
+        if missing_keys or unexpected_keys:
+            logger.warning(
+                "Loaded state_dict (strict=True) but with mismatched keys: missing=%d unexpected=%d",
+                len(missing_keys),
+                len(unexpected_keys),
+            )
+    except RuntimeError as e:
+        # Inform and attempt non-strict load so compatible parameters are restored.
+        logger.warning(
+            "Strict load failed (%s). Falling back to strict=False and loading compatible keys.",
+            str(e),
+        )
+        model.load_state_dict(state_dict, strict=False)
+        ckpt_keys = set(state_dict.keys())
+        own_keys = set(model.state_dict().keys())
+        missing_keys = own_keys - ckpt_keys
+        unexpected_keys = ckpt_keys - own_keys
+        logger.info(
+            "After non-strict load: missing keys=%d, unexpected keys=%d",
+            len(missing_keys),
+            len(unexpected_keys),
+        )
 
     del state_dict
     t_end = time.time()
     logger.info(
         "Load model, Time usage:\n\tIO: {}, initialize parameters: {}".format(
-            t_ioend - t_start, t_end - t_ioend))
+            t_ioend - t_start, t_end - t_ioend
+        )
+    )
 
     return model
 
+
 def parse_devices(input_devices):
-    if input_devices.endswith('*'):
+    if input_devices.endswith("*"):
         devices = list(range(torch.cuda.device_count()))
         return devices
 
     devices = []
-    for d in input_devices.split(','):
-        if '-' in d:
-            start_device, end_device = d.split('-')[0], d.split('-')[1]
-            assert start_device != ''
-            assert end_device != ''
+    for d in input_devices.split(","):
+        if "-" in d:
+            start_device, end_device = d.split("-")[0], d.split("-")[1]
+            assert start_device != ""
+            assert end_device != ""
             start_device, end_device = int(start_device), int(end_device)
             assert start_device < end_device
             assert end_device < torch.cuda.device_count()
@@ -212,8 +302,7 @@ def parse_devices(input_devices):
             assert device < torch.cuda.device_count()
             devices.append(device)
 
-    logger.info('using devices {}'.format(
-        ', '.join([str(d) for d in devices])))
+    logger.info("using devices {}".format(", ".join([str(d) for d in devices])))
 
     return devices
 
@@ -231,8 +320,8 @@ def extant_file(x):
 
 def link_file(src, target):
     if os.path.isdir(target) or os.path.isfile(target):
-        os.system('rm -rf {}'.format(target))
-    os.system('ln -s {} {}'.format(src, target))
+        os.system("rm -rf {}".format(target))
+    os.system("ln -s {} {}".format(src, target))
 
 
 def ensure_dir(path):
@@ -242,9 +331,10 @@ def ensure_dir(path):
             time.sleep(sleeptime)
             os.makedirs(path)
         except:
-            print('conflict !!!')
+            print("conflict !!!")
 
 
 def _dbg_interactive(var, value):
     from IPython import embed
+
     embed()
