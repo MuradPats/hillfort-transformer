410b415fb6a73581e63de880aa89d6cf4bc8bf21
SanderSaska <sander.saska@gmail.com>
2026-01-23 16:40:45 +0200

    Reduced resource allocation

--- scripts/run_preprocessing.sh
+++ scripts/run_preprocessing.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=/gpfs/helios/home/sandersa/transformers/slurm_outputs/slurm-%x.%j.out
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH -t 02:00:00
-#SBATCH --mem=6G
+#SBATCH --mem=3G
 
 # Uncomment and set your email if you want notifications
 ##SBATCH --mail-type=END,FAIL
